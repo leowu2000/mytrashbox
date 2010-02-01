4d2cac5f08f10dd15c420b5d8fc07248eaae4186
devilcony <devilcony@6e1a95b2-03dc-11df-804c-772d9fe11005>
2010-02-01 02:24:16 +0000

    数据库脚本修改

--- TechOA/docs/initview.sql
+++ TechOA/docs/initview.sql
@@ -1,9 +1,9 @@
 /*人员基本信息报表*/
 create or replace view VIEW_EMP as
 select DISTINCT
-a.name,a.empcode,b.name as depart,a.mainjob,a.secjob,a.level,a.email,a.blog,a.selfweb,a.stcphone,a.mobphone,a.address,a.post,c.name as major,d.name as degree,e.name as pro 
+a.name,a.code,b.name as depart,a.mainjob,a.secjob,a.level,a.email,a.blog,a.selfweb,a.stcphone,a.mobphone,a.address,a.post,c.name as major,d.name as degree,e.name as pro 
         from employee a,department b,dict c,dict d,dict e
-	where b.code=a.departcode and c.code=a.majorcode and d.code=a.degreecode and e.code=a.procode
+	where b.code=a.departcode and c.code=a.majorcode and d.code=a.degreecode and e.code=a.procode;
 	
 
 /*职工考勤记录*/
@@ -12,11 +12,11 @@ create or replace view VIEW_WORKCHECK as
 select DISTINCT
 	a.*,b.DEPARTCODE,b.NAME as EMPNAME,c.NAME as RESULTNAME
 	from WORKCHECK a,EMPLOYEE b,DICT c
-	where b.EMPCODE=a.EMPCODE and c.CODE=a.CHECKRESULT
+	where b.CODE=a.EMPCODE and c.CODE=a.CHECKRESULT;
 
 /*工作报告记录*/
 
 create or replace view VIEW_WORKREPORT as
 select a.*,b.name as PJNAME,c.name as STAGENAME,d.name as EMPNAME 
 	from WORKREPORT a, PROJECT b, DICT c, EMPLOYEE d
-	where b.CODE=a.PJCODE and c.CODE=a.STAGECODE and d.EMPCODE=a.EMPCODE
+	where b.CODE=a.PJCODE and c.CODE=a.STAGECODE and d.CODE=a.EMPCODE;
